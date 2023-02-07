8ea173d193ffef919a6a57c9448c92ff8e2ece73
Frank <f.neumann.berlin@googlemail.com>
2023-02-07 12:29:13 +0100

    Base_url

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.weatherapp
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weatherapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvTemp.text = getString(R.string.temp_template,20)
+
+
+        binding.btnShowWeather.setOnClickListener {
+            MainScope().launch {
+                val weatherData:WeatherData
+                var bitmap:Bitmap? = null
+                withContext(Dispatchers.IO){
+                    weatherData = WeatherUtil.getWeatherData(binding.etCity.text.toString(),getString(R.string.api_key))
+                }
+                binding.tvTemp.text = getString(R.string.temp_template,(weatherData.temp-273.15).toInt())
+                binding.tvDesc.text = weatherData.description
+
+                if(weatherData.icon.isNotBlank()){
+                    withContext(Dispatchers.IO){
+                        bitmap = WeatherUtil.getImage(weatherData.icon)
+                    }
+                }
+
+                binding.imgWeather.setImageBitmap(bitmap)
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/example/weatherapp/WeatherUtil.kt
+++ app/src/main/java/com/example/weatherapp/WeatherUtil.kt
@@ -1,26 +1,78 @@
 package com.example.weatherapp
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import org.json.JSONObject
 import java.net.URL
+import java.text.MessageFormat
 import javax.net.ssl.HttpsURLConnection
 
 object WeatherUtil {
 
     private val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q={0}&lang=de&appid={1}"
+    private val TAG = "json"
+    private val NAME = "name"
+    private val MAIN = "main"
+    private val TEMP = "temp"
+    private val WEATHER = "weather"
+    private val DESCRIPTION = "description"
+    private val ICON = "icon"
 
 
+    fun getWeatherData(city: String, key: String): WeatherData {
+        var name = ""
+        var description = ""
+        var icon = ""
+        var temp = 0.0
+        val jsonString = getFromServer(MessageFormat.format(BASE_URL, city, key))
+        Log.d(TAG, "getWeatherData: $jsonString")
+        val rootObject = JSONObject(jsonString)
+        if(rootObject.has(NAME))
+            name = rootObject.getString(NAME)
+        if(rootObject.has(WEATHER)){
+            val weatherArray = rootObject.getJSONArray(WEATHER)
+            if(weatherArray.length()>0){
+                val weatherObject = weatherArray.getJSONObject(0)
+                if(weatherObject.has(DESCRIPTION))
+                    description = weatherObject.getString(DESCRIPTION)
+                if(weatherObject.has(ICON))
+                    icon = weatherObject.getString(ICON)
+            }
 
-    fun getFromServer(urlString:String):String{
+        }
+        if (rootObject.has(MAIN)){
+            val mainObject = rootObject.getJSONObject(MAIN)
+            if (mainObject.has(TEMP))
+                temp = mainObject.getDouble(TEMP)
+        }
+        return WeatherData(name, description, icon, temp)
+
+    }
+
+    private fun getFromServer(urlString: String): String {
         val url = URL(urlString)
         var jsonString: String = "{}"
         val connection = url.openConnection() as HttpsURLConnection
         val responseCode = connection.responseCode
-        if(responseCode == HttpsURLConnection.HTTP_OK){
+        if (responseCode == HttpsURLConnection.HTTP_OK) {
             jsonString = connection.inputStream.bufferedReader().use {
                 it.readText()
             }
         }
         connection.disconnect()
 
-        return  jsonString
+        return jsonString
+    }
+
+    fun getImage(imageName:String): Bitmap? {
+        val url = URL("https://openweathermap.org/img/w/$imageName.png")
+        val connection = url.openConnection() as HttpsURLConnection
+        var bitmap:Bitmap? = null
+        if(connection.responseCode == HttpsURLConnection.HTTP_OK) {
+            bitmap = BitmapFactory.decodeStream(connection.inputStream)
+            connection.disconnect()
+        }
+        return bitmap
     }
 }
