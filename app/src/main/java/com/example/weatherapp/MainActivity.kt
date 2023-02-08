7a34f22d59763f53af20fc968306bc1192a268da
Frank <f.neumann.berlin@googlemail.com>
2023-02-08 08:49:14 +0100

    Base_url

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.weatherapp
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.weatherapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -14,6 +19,25 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private val isNetworkAvailable: Boolean
+        get() = (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).run {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                val networkCapabilities = activeNetwork?: return false
+                val actNw = getNetworkCapabilities(networkCapabilities)?: return false
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)||
+                        actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)||
+                        actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            }else{
+                activeNetworkInfo?.run {
+                    type == ConnectivityManager.TYPE_WIFI ||
+                            type == ConnectivityManager.TYPE_ETHERNET||
+                            type == ConnectivityManager.TYPE_MOBILE
+
+                } ?: false
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,6 +46,12 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.btnShowWeather.setOnClickListener {
+
+            if(!isNetworkAvailable){
+                Toast.makeText(this, "Kein Netzwerk verfügbar", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             MainScope().launch {
                 val weatherData:WeatherData
                 var bitmap:Bitmap? = null
